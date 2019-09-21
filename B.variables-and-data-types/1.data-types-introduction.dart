4de7db6c8710f112023f124699b7b4f6b609dc3c
Uday Hiwarale <thatisuday@gmail.com>
2019-09-21 20:39:08 +0530

    Update 1.data-types-introduction.dart

--- B.variables-and-data-types/1.data-types-introduction.dart
+++ B.variables-and-data-types/1.data-types-introduction.dart
@@ -3,16 +3,19 @@
  * Default value of a variable is always null.
  */
 void main() {
-  int myNumber;
-  double myDouble;
-  String myString;
-
-  // We can optionally assign `null` to a variable during variable initialization
-  // or at anytime do declare that a variable has a no value
+  
+  // variables without a Data Type must be defined with Data Type identifiers
+  int myNumber; // `int` Data Type is for integer values
+  double myDouble; // `double` Data Type is for floating-point numbers
+  String myString; // `String` Data Type is for string values (sequence of characters)
+  
+  // variables without an initial have `null` values
+  // we can also assign `null` to a variable at variable declaration or at anytime
+  // `null` signifies no-value or empty-value
   bool myBoolean = null;
 
-  print( myNumber );
-  print( myDouble );
-  print( myString );
-  print( myBoolean );
+  print( myNumber );  // null
+  print( myDouble );  // null
+  print( myString );  // null
+  print( myBoolean ); // null
 }
