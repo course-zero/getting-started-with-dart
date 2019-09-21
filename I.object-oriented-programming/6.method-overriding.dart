bd4c722899103b7c174cbe9dcbd57e2513467b1a
Uday Hiwarale <thatisuday@gmail.com>
2019-09-22 01:46:43 +0530

    Update 6.method-overriding.dart

--- I.object-oriented-programming/6.method-overriding.dart
+++ I.object-oriented-programming/6.method-overriding.dart
@@ -1,12 +1,30 @@
+/**
+ * When both `sub-class` and `super-class` has a method with common name,
+ * then method in the `sub-class` is used instead.
+ *
+ * This is called method overriding as method in the `sub-class` is preferred over method in the `super-class`,
+ * which is more like overriding the functionality of the `super-class` method.
+ *
+ * If the intention of the method overriding is to extend the functionality of the `super-class` method,
+ * then original method should be used from inside the method in the `sub-class` using `super` keyword.
+ *
+ * Dart recommended to annotate an overriding method using `@override` annotation.
+ *
+ * An `abstract` class can not be instantiated. It can only be inherited.
+ */
 void main() {
+  
   // create Employee object
   var e = Employee( 'Ross', 'Geller', 1000 );
   print( e );
+  
 }
 
-// Person class with basic details
-// Since Person is abstract, it can not be instantiated
+// Person object contains basic personal details
+// Since `Person` is abstract, it can not be instantiated
 abstract class Person {
+  
+  // instance variables
   String firstName, lastName;
 
   // default constructor
@@ -17,25 +35,29 @@ abstract class Person {
     return '${ this.firstName } ${ this.lastName }';
   }
 
-  // return String format of the Person object
+  // return `String` value format of the `Person` object
   @override
+  // override `toString` method of `Object` class
   String toString() {
     return 'Person: ${ this.getFullName() }';
   }
 }
 
-// Employee class shares feature of Person class
+// `Employee` class shares feature of `Person` class
 class Employee extends Person {
   int salary;
 
   // default constructor
   Employee( String firstName, String lastName, this.salary ): super( firstName, lastName );
 
-  // return String format of the `Employee` object
-  // override `toString` of Person class by calling it
+  // return `String` value format of the `Employee` object
+  // overrides `toString` method of `Person` class
   @override
   String toString() {
-    var rprPerson = super.toString(); // Person object String representation
+    
+    // execute parent `toString` method
+    var rprPerson = super.toString(); // `Person` object String representation
+    
     return 'Employee: (${ rprPerson }) with salary ${ this.salary }'; 
   }
 }
