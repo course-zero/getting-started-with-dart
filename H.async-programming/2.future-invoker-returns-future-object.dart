3b32a9466599f73a73fdc5118905af65f929e014
Uday Hiwarale <thatisuday@gmail.com>
2019-09-22 00:38:08 +0530

    Update 2.future-invoker-returns-future-object.dart

--- H.async-programming/2.future-invoker-returns-future-object.dart
+++ H.async-programming/2.future-invoker-returns-future-object.dart
@@ -1,20 +1,31 @@
+/**
+ * An `invoker` function can also return another `Future` object.
+ * In that case, original `Future` object is not resolved until returned `Future` object resolves.
+ *
+ * When returned `Future` object is resolved, main `Future` object is also resolved.
+ * When returned `Future` object is rejected, main `Future` object is also rejected.
+ */
 void main() {
 
-  // create Future object with an invoker that returns another Future object
+  // create `Future` object with an `invoker` that returns another `Future` object
   Future<String> future = Future( () {
 
-    // return Future object that returns a String
+    // return `Future` object that returns a `String` object
     return Future( () => "Hello from the future!" );
   } );
 
   // just a simple statement
   print( '1. Future object created.' );
 
-  // use only `then` method to register success and error callbacks
+  // we can use only `then` method to register success and error callbacks
   future.then( ( result ) {
+    
     print( '2. Sucess: $result' );
+    
   }, onError: ( error ) {
+    
     print( '2. Error: $error' );
+    
   } );
 
   // just a simple statement
