c43e9744e63fb2f6cf196f28c2d0b1a738383956
Uday Hiwarale <thatisuday@gmail.com>
2019-09-22 01:06:31 +0530

    Update 1.class-introduction.dart

--- I.object-oriented-programming/1.class-introduction.dart
+++ I.object-oriented-programming/1.class-introduction.dart
@@ -1,5 +1,15 @@
 /**
- * A class is created using `class` keyword.
+ * A class is used to construct objects like map, lists, sets, etc.
+ * A Dart class is created using `class` keyword.
+ * A Dart class automatically inherits from `Object` class.
+ * A Dart class constructor has the same name as the class.
+ * Any properties defined without a `static` keyword are instance properties.
+ *
+ * An object from a class is created using `ClassName()` or `new ClassName()` syntax.
+ * Sicne Dart v2, `new` keyword to create an object is optional.
+ *
+ * `this` keyword inside an instance method points to the object itself.
+ * `@override` annotation is used to mark a method that is overriding the method defined in the inherited class.
  */
 void main() {
 
@@ -12,20 +22,30 @@ void main() {
   print( "ross object: $ross" );
 }
 
-// simple Persona class
+// simple Person class
 class Person {
-  static String company = 'AWESOME LLC.'; // static variables are accessed on the class
-  String firstName, lastName; // instance variables with `null` value
-  var age = 18; // instance variable with initial `18` value
+  
+  // A static variables are accessed on the class
+  static String company = 'AWESOME LLC.';
+  
+  // instance variables with `null` value
+  String firstName, lastName;
+  
+  // instance variable with initial `18` value
+  var age = 18;
 
   // instance methods
   String getFullName() {
     return this.firstName + " " + this.lastName; // `this` points to the object itself
   }
 
-  // instance method to print string representation of the object
+  // `toString` instance method to print string representation of the object
+  // it overrides `toString` method defined in `Object` class
   @override
   String toString() {
+    
+    // since `company` is a static variable, it should be accessed on `Person` class
     return "Name: ${ this.getFullName() }, Age: ${ this.age }, Company: ${ Person.company }";
   }
+  
 }
