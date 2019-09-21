e43270e45dee3b8aa469b2b997e22ad2a49d3a87
Uday Hiwarale <thatisuday@gmail.com>
2019-09-21 22:23:02 +0530

    Update 3.non-null-selector.dart

--- D.conditional-statements/3.non-null-selector.dart
+++ D.conditional-statements/3.non-null-selector.dart
@@ -3,10 +3,10 @@
  */
 void main() {
   var x = 10;
-  int y; // null value on init
+  int y; // y has `null` initial value
 
-  var safe_x = x ?? 0; // since `x` is not null, get value of x which is 10
-  var safe_y = y ?? 0; // since `y` is null, get 0
+  var safe_x = x ?? 0; // since `x` is not null, safe_x get the value of `x` which is `10`
+  var safe_y = y ?? 0; // since `y` is `null`, safe_y gets `0` value
 
   print( "x + y = ${ safe_x + safe_y }" );
 }
