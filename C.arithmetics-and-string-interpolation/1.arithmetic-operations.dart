6f9532c86e5446e5debd07dba2b6842c5f32a69d
Uday Hiwarale <thatisuday@gmail.com>
2019-09-21 21:56:02 +0530

    Update 1.arithmetic-operations.dart

--- C.arithmetics-and-string-interpolation/1.arithmetic-operations.dart
+++ C.arithmetics-and-string-interpolation/1.arithmetic-operations.dart
@@ -1,19 +1,28 @@
 /**
- * Arithmetic operations.
+ * Dart supports all arithmetic operators.
  */
 void main() {
 
-  var x = 11;
-  var y = 2;
-
-  var result = x + y;
+  var x = 11; // `x` has `int` type
+  var y = 2;  // `y` has `int` type
+  
+  // perform arithmetic operation and save the result in a variable
+  var result = x + y; // `result` has `int` type
 
   print("result = ${ result }"); // Addition
+  
   print("x - y = ${ x - y }"); // Subtraction
+  
   print("x * y = ${ x * y }"); // Multiplication
+  
+  // return type of a division is always a `double` value
   print("x / y = ${ x / y } and is a double ${ (x / y) is double }"); // Division
+  
+  // return type of a division with quotient is always a `int` value
   print("x ~/ y = ${ x ~/ y }"); // Quotient
+  
   print("x % y = ${ x % y }"); // Modulus
+  
   print("x++ = ${ x++ }"); // Increment after return
   print("--y = ${ --y }");// Decrement before return
 }
