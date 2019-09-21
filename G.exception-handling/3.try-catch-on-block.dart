b13dcfc65373db72349519b2f39a7c9b8d14ea0c
Uday Hiwarale <thatisuday@gmail.com>
2019-09-21 23:57:04 +0530

    Update 3.try-catch-on-block.dart

--- G.exception-handling/3.try-catch-on-block.dart
+++ G.exception-handling/3.try-catch-on-block.dart
@@ -1,7 +1,16 @@
+/**
+ * Apart from `try/catch/finally` block, Dart also provides `on` block.
+ * This block is used to capture a specific type of exception.
+ * There can be many `on` blocks in a `try-catch` statement.
+ *
+ * The `on` block can be used in conjugation with `catch` to
+ * capture the exception raised in `try` block.
+ */
 void main() {
+  
   try {
     
-    var result = 100 ~/ 0;
+    var result = 100 ~/ 0; // throws `IntegerDivisionByZeroException` exception
     print( 'result: $result' );
 
   } on IntegerDivisionByZeroException {
@@ -9,14 +18,14 @@ void main() {
     // execute when exception type is: IntegerDivisionByZeroException
     print("Error: can not divide by 0.");
 
-  } on FormatException {
+  } on FormatException catch( e ) {
 
-    // execute when exception type is: IntegerDivisionByZeroException
-    print("Error: format is not correct.");
+    // execute when exception type is: FormatException
+    print("Error: format is not correct, $e");
 
   } catch( e ) {
 
-    // this block executes if none of the `on` block catches exception
+    // this block executes if none of the `on` block catches the exception
     print( e );
 
   } finally {
@@ -24,4 +33,5 @@ void main() {
     print( 'Job Done!' );
 
   }
+  
 }
