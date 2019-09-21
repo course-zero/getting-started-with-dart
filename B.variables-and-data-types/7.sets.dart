ac2b263401a35df8be201d48abbbe57ec1c195cf
Uday Hiwarale <thatisuday@gmail.com>
2019-09-21 21:51:47 +0530

    Update 7.sets.dart

--- B.variables-and-data-types/7.sets.dart
+++ B.variables-and-data-types/7.sets.dart
@@ -1,20 +1,22 @@
 /**
  * A set is like a List data structure but it can only contain unique values.
- * A set can be created with {} literal syntax or using Set class.
+ * A set can be created with {} literal syntax or using `Set` class.
  */
 void main() {
 
-  final name = 'Gandhi';
-  print( name );
-
-  // set using literal syntax and initial values
-  // any duplicate values will be removed
-  var fruits = { 'Apple', 'Orange', 'Banana', 'Orange' };
+  // creating an empty set
+  var myEmptySet = new Set();
+  var myEmptyStringSet = new Set<String>(); // or `Set<String> myEmptyStringSet = new Set();` declaration
+  
+  // creating a set with initial values
+  // if all initial values belong to same the Data Type, a set with fixed Data Type is created
+  var fruits = { 'Apple', 'Orange', 'Banana', 'Orange' }; // duplicate values are removed
   fruits.add( 'Mango' );
-  // fruits.add( 100 ); :=> INVALID Data Type
+  // fruits.add( 100 ); // invalid operation as `fruits` has `Set<String>` type
   print( 'fruits: $fruits \n' ); 
 
-  // create empty set
+  // we can only create an empty set using `Set` class.
+  // `{}` syntax is reserved to create empty maps.
   var otherFruits = new Set<String>();
   otherFruits.add( 'Grapes' );
   otherFruits.addAll( fruits ); // add elements from a set
