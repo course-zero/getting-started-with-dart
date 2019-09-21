a424c0233804e7c194c58ab311cf4323e24303da
Uday Hiwarale <thatisuday@gmail.com>
2019-09-22 00:57:15 +0530

    Update 5.async-await.dart

--- H.async-programming/5.async-await.dart
+++ H.async-programming/5.async-await.dart
@@ -1,5 +1,17 @@
 import 'package:http/http.dart' as http;
 
+/**
+ * Using `then` chaining is not discouraged but it can soon get complicated to implement.
+ * Dart provides `async/await` syntax to handle `Future` objects in synchronous code fashion.
+ *
+ * When a function is marked with `async`, it returns a `Future` object.
+ * When `await` keyword is placed before a `Future` object in that function,
+ * it blocks the execution of further code in that function until that `Future` object is resolved.
+ *
+ * Since there is no `catchError` method to catch errors in the `Future` objects,
+ * Dart throws an exception when a `Future` object is rejected.
+ * Hence code inside an `async` function should be handled with `try-catch` block.
+ */
 void main() {
 
   // call `getUsers` method which returns a `Future` object
@@ -13,21 +25,27 @@ void main() {
 
 }
 
-// this method returns a Future object because it is labelled with `async` keyword
-// this this Future object always return a value, only `then` method is invoked by it
+// this method returns a `Future` object because it is marked with `async` keyword
+// Since we are using `try-catch` block, returned `Future` object always resolves.
+// `Future` object upon resolution returns an `int` object
 Future<int> getUsers () async {
 
   try {
-    // await waits until Future object is resolved or rejected
+    
+    // `await` waits until the `Future` object returned by `http.get()` is resolved or rejected
+    // this `Future` object is rejected when network request fails to retrieve data
+    // if this `Future` object is rejected, an exception will be thrown
     final response = await http.get( 'https://reqres.in/api/users/23' );
 
-    // below statements are blocked until Future object returned by `http.get`
-    // returns a value (resolves) which happens when network request returns a response
+    // below statements are blocked until the above statement lock is released
     return response.statusCode; // returns `int` value
+    
   } catch( e ) {
-    // this block is executed when `http.get` fails to retrieve a response due to network failure
+    
+    // this block is executed when an exception is thrown in `try` block
     print("Network Failure: $e");
-    return 0; // return 0
+    return 0; // return `0` value (int)
+    
   }
 
 }
