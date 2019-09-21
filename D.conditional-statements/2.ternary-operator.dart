0baaecfa95ad9f0aa2f93ff5a1248f54fceffbbc
Uday Hiwarale <thatisuday@gmail.com>
2019-09-21 22:21:55 +0530

    Update 2.ternary-operator.dart

--- D.conditional-statements/2.ternary-operator.dart
+++ D.conditional-statements/2.ternary-operator.dart
@@ -1,16 +1,25 @@
 /**
- * Ternary operator are used to evaluate a condition just like if/else
- * but on a single line. The `result` of the executed statement can be
- * ignored or saved into a variable.
+ * Ternary operator are used to evaluate a condition just like if/else but on a single line.
+ * 
+ * condition ? expr1 : expr2;
+ * The `result` of the executed expression can be ignored or saved into a variable.
  */
 void main() {
 
   // execute a statement
   var condition = true;
   condition ? print( 'when condition is "true"' ) : print( 'when condition is "false"' );
+  
+  // save the selected value in a variable
+  var value = ( 1 > 2 ) ? 'STUPID' : 'SMART';
+  print( "value = $value" );
 
-  // save the result in a variable
-  var result = ( 1 > 2 ) ? 'STUPID' : 'SMART';
-  print( "result = $result" );
+  // save the result of an evaluated expression in a variable
+  var name = true ? getFirstName() : getLastName();
+  print( "name = $name" );
 
 }
+
+// fat arrow functions
+String getFirstName() => "John";
+String getLastName() => "Doe";
