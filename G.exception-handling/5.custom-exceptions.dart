d17fa916498d8b4652ea72d13002ebbd883fe627
Uday Hiwarale <thatisuday@gmail.com>
2019-09-22 00:02:57 +0530

    Update 5.custom-exceptions.dart

--- G.exception-handling/5.custom-exceptions.dart
+++ G.exception-handling/5.custom-exceptions.dart
@@ -1,15 +1,31 @@
 import 'dart:math';
 
+/**
+ * When it comes to throwing exception manually, Dart provide some built-in classes.
+ * An exception class implements built-in `Exception` class.
+ *
+ * To throw an custom exception, we need to create a custom class that implements `Exception` class.
+ */
 void main() {
+  
   try {
+    
     var result = squareRoot(-4);
+    
     print( "result: $result" );
+    
   } on NegativeSqrtException catch( e ) {
+    
     print( "Oops, Negative Number: $e" );
+    
   } catch( e ) {
+    
     print( e );
+    
   } finally {
+    
     print( 'Job Done!' );
+    
   }
 }
 
@@ -17,16 +33,22 @@ void main() {
 class NegativeSqrtException implements Exception {
   @override
   String toString() {
+    
     return 'Sqauare root of negative number is not allowed.';
+    
   }
 }
 
 // get square root of a positive number
 num squareRoot( int i ) {
   if( i < 0 ) {
-    // NegativeSqrtException is custom class
+    
+    // throw `NegativeSqrtException` exception
     throw NegativeSqrtException();
+    
   } else {
+    
     return sqrt( i );
+    
   }
 }
