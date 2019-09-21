622325ce4f79297bbf24706080c4e4913d52a8b5
Uday Hiwarale <thatisuday@gmail.com>
2019-09-21 20:44:50 +0530

    Update 2.data-types-with-values.dart

--- B.variables-and-data-types/2.data-types-with-values.dart
+++ B.variables-and-data-types/2.data-types-with-values.dart
@@ -3,20 +3,29 @@
  * Once a variable is defined, it should contain only that data type.
  */
 void main() {
-  var age = 27;
+  
+  // Dart can infer the Data Type of a variable from its initial value
+  var age = 27; // `age` variable has `int` type because `27` is an integer
+  
+  // if a variable of `double` Data Type is assigned with `int` value,
+  // Dart converts the value to suitable floating-point number
   double grades = 9; // converted to `9.0`
+  
+  // strings in Dart can be defined inside single or double quotes
   var favColor="Green";
-  String favFruit='Banana'; // string can also be defined in single-quotes
-  bool isMarried; // mo initial value, must state a data type
+  String favFruit='Banana';
+  
+  // since `isMarried` variable has no initial value (initial `null` value), we must declare its Data Type
+  bool isMarried; // `isMarried` can contain `true`, `false` or `null` value
 
-  // assign some values
+  // we can assign new values to exisiting variables
   favFruit='Mango';
-  isMarried=false; // Use `true` is for boolean true
-  grades=10; // converted to 10.0 by Dart
+  isMarried=false;
+  grades=10; // converted to 10.0 as `grades` has `double` Data Type
   
   // Error: A value of type 'int' can't be assigned to a variable of type 'String'.
   // favColor=100;
-
+  
   print(age);
   print(grades);
   print(favColor);
