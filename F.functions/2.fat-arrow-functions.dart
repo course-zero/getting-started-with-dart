997704f99a2d402a707ebd76b4db290b5db077e5
Uday Hiwarale <thatisuday@gmail.com>
2019-09-21 23:14:57 +0530

    Update 2.fat-arrow-functions.dart

--- F.functions/2.fat-arrow-functions.dart
+++ F.functions/2.fat-arrow-functions.dart
@@ -1,23 +1,25 @@
 /**
- * Fat arrow functions or lambda expessions is a clean way to
- * write function expression on a single line.
+ * Fat-arrow function or lambda expession is a clean way to write a function expression on a single line.
+ * Fat-arrow functions doesn't have a statements body `{}`.
+ * The statement body is replaced with `=>` fat-arrow which points to a single statement.
+ *
+ * This statement is executed when the fat-arrow function is called and
+ * it always returns the result of the statement execution.
  */
 void main() {
 
-  // fat arrow function doesn't have a statements body `{}`
-  // statement after `=>` automatically returns a value
-  // this is the way to write fat-arrow function that returns `void`
+  // `print( "Hello $name." )` returns a `void` object
   void sayHello ( String name ) => print( "Hello $name." );
 
   // automatically returns result of expression `a * a`
   // Data Types are inferred at runtime
   var getSquare = ( a ) => a * a;
 
-  // same as above but with explicit arguments Data Types
+  // same as the above but with explicit Data Type declaration for the arguments
   var add = ( num a, num b ) => a + b;
 
-  sayHello( "John" ); // this function does not return anything
+  sayHello( "John" );    // this function does not return anything
   print( getSquare(3) ); // print returned value
-  print( add(1, 2) ); // print returned value
+  print( add(1, 2) );    // print returned value
 
 }
