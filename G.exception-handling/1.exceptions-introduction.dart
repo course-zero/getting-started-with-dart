49c51c1ab3cadfaec2fa1bddf2171aa0d72e16c3
Uday Hiwarale <thatisuday@gmail.com>
2019-09-21 23:47:40 +0530

    Update 1.exceptions-introduction.dart

--- G.exception-handling/1.exceptions-introduction.dart
+++ G.exception-handling/1.exceptions-introduction.dart
@@ -1,13 +1,17 @@
+/**
+ * An exception is like an error thrown by the Dart runtime to indicate an error.
+ */
 void main() {
+  
   // this statement executes
   print( 'Program started!' );
 
-  // this statement throws the exception and terminates the program
+  // this statement throws an exception and terminates the program.
   var result = 100 ~/ 0;
 
-  // after exception is thrown, none of the below code executes
+  // after an exception is thrown, none of the statements execute.
   print( "result: $result" );
 
-  // this statement does not executes
+  // this statement does not execute
   print( 'Program terminated successfully!' );
 }
