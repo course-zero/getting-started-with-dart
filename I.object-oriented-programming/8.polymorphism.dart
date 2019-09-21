2410cd67e61ee3061c6c4d62826714a1fb886c0e
Uday Hiwarale <thatisuday@gmail.com>
2019-09-22 01:59:47 +0530

    Update 8.polymorphism.dart

--- I.object-oriented-programming/8.polymorphism.dart
+++ I.object-oriented-programming/8.polymorphism.dart
@@ -1,18 +1,31 @@
+/**
+ * Polymorphism is the ability of an object to take any form.
+ * Everything in Dart is an object and all objects are inherited from `Object` class.
+ * Hence for example, a `String` object is both type of `String` and `Object.
+ *
+ * An object of a class that `inherits` another class, can be represented in both the class types.
+ * Also, an object of a class that `implements` another class, can also be represented in both the class types.
+ *
+ * We use `object.runtimeType` to get actual type of the object at runtime.
+ */
 void main() {
-  // create Person and Employee object
+  // create `Person` and `Employee` objects
   var p = Person( 'Joey', 'Tribbiani' );
   var e = Employee( 'Ross', 'Geller', 1000 );
   
+  // We can call `fullName` function with `p` because it accepts argument of type `Person`
   print( 'Person p full name: ${ fullName( p ) }' );
+  
+  // We can call `fullName` function with `e` because `e` is also a type of `Person`
   print( 'Employee e full name: ${ fullName( e ) }' );
 }
 
-// getFullName of a Person or an Employee
+// `getFullName` of a `Person` or an `Employee` object
 String fullName(Person person) {
   return '${ person.firstName } ${ person.lastName }';
 }
 
-// Person class with basic details
+// A `Person` object contains basic personal details
 class Person {
   String firstName, lastName;
 
@@ -20,7 +33,7 @@ class Person {
   Person( this.firstName, this.lastName );
 }
 
-// Employee class shares feature of Person class
+// `Employee` class shares feature of the `Person` class
 class Employee extends Person {
   int salary;
 
