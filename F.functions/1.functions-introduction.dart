cda47fae68fa19a37e20b9c06bae569dfe6a8864
Uday Hiwarale <thatisuday@gmail.com>
2019-09-21 23:07:38 +0530

    Update 1.functions-introduction.dart

--- F.functions/1.functions-introduction.dart
+++ F.functions/1.functions-introduction.dart
@@ -1,37 +1,40 @@
 /**
- * In Dart, function act like a method or first-class objects.
+ * In Dart, functions can be defined in the file scope or inside another function.
+ * Since everything is an object in Dart, function behaves like first-class objects.
+ * Functions can be stored in a variable, passed as an argument or returned as a value.
+ *
+ * Functions defined in the file scope, like `main` function are hoisted.
+ * Hence we can call one function from another despite their order on appearance in the file.
  */
 void main() {
 
-  // `sayHello` function defined like a method
-  // with return Data Type of `String`
+  // `sayHello` function is defined like a method with `String` return value
   String sayHello ( String name ) {
-    var greeting = greet();
+    var greeting = greet(); // call `greet` function
     return "$greeting, $name.";
   };
 
-  // function defined without a name, AKA `anonymous function`
+  // anonymous functions` are function defined without a name
   // `getSum` variable has `num Function(num, num)` type
-  // AVOID: complicated syntax
+  // AVOID: (we should avoid complex type declaration, Dart can infer type automatically)
   num Function(num, num) getSum = ( num a, num b ) {
     return a + b;
   };
 
-  // function defined without a name
-  // `getDivision` variable is of type `Function`
-  // PREFERRED: easier to read, data types are inferred at runtime
+  // `getDivision` variable has `dynamic Function(dynamic, dynamic)` type
+  // PREFERRED: easier to read, data types are inferred automatically from arguments and return types
   var getDivision = ( a, b ) {
     return a ~/ b;
   };
 
-  // `print` is also a function
+  // `print` is also a function (built-in)
   print( sayHello( "John" ) );
   print( getSum( 22, 11 ) );
   print( getDivision( 22, 11 ) );
 
 }
 
-// function returns a String object
+// function returns a `String` value
 String greet() {
-  return "Good Morning"; // can change depending upon time
+  return "Good Morning";
 }
