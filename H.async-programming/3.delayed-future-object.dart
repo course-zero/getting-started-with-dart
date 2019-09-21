bdfbd972dcefc4780a45638e5bca22c8e162c792
Uday Hiwarale <thatisuday@gmail.com>
2019-09-22 00:39:36 +0530

    Update 3.delayed-future-object.dart

--- H.async-programming/3.delayed-future-object.dart
+++ H.async-programming/3.delayed-future-object.dart
@@ -1,6 +1,10 @@
+/**
+ * Dart provides `delayed` named constructor on `Future` object that executes
+ * invoker function after some delay.
+ */
 void main() {
 
-  // create future object that executes the `callback` function after 3 seconds
+  // create future object that executes the `invoker` function after `3` seconds
   Future<String> future = Future.delayed( Duration(seconds: 3), () => "Hello from the future." );
 
   // just a simple statement
@@ -8,9 +12,13 @@ void main() {
 
   // react to future objects resolution or rejection
   future.then( ( result ) {
+    
     print( '2. Sucess: $result' );
+    
   } ).catchError( ( error ) {
+    
     print( '2. Error: $error' );
+    
   } );
 
   // just a simple statement
