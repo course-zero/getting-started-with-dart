554fc96251132ca9ef35a99a6f810b9939363649
Uday Hiwarale <thatisuday@gmail.com>
2019-09-22 01:25:17 +0530

    Update 4.getters-and-setters.dart

--- I.object-oriented-programming/4.getters-and-setters.dart
+++ I.object-oriented-programming/4.getters-and-setters.dart
@@ -1,3 +1,18 @@
+/**
+ * A getter function is called when it is accessed like an instance variable on the object.
+ * A setter function is called when a value is assigned to object with the same variable name.
+ *
+ * We use `get` keyword to define a `getter` function.
+ * Getter function does not take any arguments and returns a value.
+ * Hence getter function does not have parentheses. They can also be fat-arrow functions for simplicity.
+ * -> String get funcName { return_statement; }
+ * -> String get funcName => return_statement;
+ *
+ * We use `set` keyword to define a `setter` function.
+ * A setter function takes one arguments which is the value user is trying to set and returns no value (void).
+ * -> void set funcName () { statements; }
+ * -> void set funcName => statement;
+ */
 void main() {
 
   var ross = Person( "Ross Geller Jr." );
@@ -9,21 +24,23 @@ void main() {
 }
 
 class Person {
-  String _firstName, _lastName; // _ prefix to state private variables
-
+  
+   // _ (underscore) prefix makes variables private to the library
+  String _firstName, _lastName;
+  
+  // default constructor
   Person( String name ) {
     var nameParts = name.split(" "); // split name by a space
     this._firstName = nameParts[ 0 ];
     this._lastName = nameParts[ 1 ];
   }
 
-  // getter function for fullName property
-  // defined like function without parantheses, as it takes no arguments
+  // getter function for `fullName` property
   String get fullName {
     return "${ this._firstName } ${ this._lastName }";
   }
 
-  // setter function for fullName property
+  // setter function for `fullName` property
   void set fullName( String name ) {
     var nameParts = name.split(" "); // split name by a space
     this._firstName = nameParts[ 0 ];
