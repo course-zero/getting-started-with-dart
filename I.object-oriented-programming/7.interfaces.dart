3a1ca185c9b8731be84ebe62dd4af42670cd66b9
Uday Hiwarale <thatisuday@gmail.com>
2019-09-22 01:52:30 +0530

    Update 7.interfaces.dart

--- I.object-oriented-programming/7.interfaces.dart
+++ I.object-oriented-programming/7.interfaces.dart
@@ -1,3 +1,14 @@
+/**
+ * Interfaces are blueprints of how a class should look like.
+ * Interfaces provide a `strict` schema to define a class.
+ *
+ * In Dart, a class is also defined as interface implicitely. Hence they are called implicit interfaces.
+ * We used `implements` keyword to implement an interface on a class.
+ * Unlike inheritance, a class can implement multiple interfaces.
+ *
+ * Like inheritance, constructor function and static properties
+ * is not a part of implicit interface created by a class.
+ */
 void main() {
   // create Employee object
   var e = Employee( 'Ross', 'Geller', 1000 );
@@ -5,12 +16,13 @@ void main() {
   print( 'Employee e full name: ${ e.getFullName( e.firstName, e.lastName ) }' );
 }
 
-// abstract `Person` class can be extended or implemented
+// abstract `Person` class can only be extended or implemented
 abstract class Person {
+  
   // instance variables definition
   String firstName, lastName;
 
-  // constructor is not a part of `Person` interface
+  // constructor is not a part of the `Person` interface
   Person( this.firstName, this.lastName );
 
   // instance method definition
@@ -22,14 +34,16 @@ abstract class Person {
   static String getVersion() {
     return Person.getVersion();
   }
+  
 }
 
-// `Employee` class shares feature of `Person` class
+// `Employee` implements structure defined by `Person` class
 class Employee implements Person {
+  
   // ENFORCED: instance variables declaration from the `Person` interface
   String firstName, lastName;
 
-  int salary; // not enforced
+  int salary; // not enforced as it is missing in `Person` class
 
   // default constructor
   Employee( this.firstName, this.lastName, this.salary );
@@ -40,4 +54,5 @@ class Employee implements Person {
   String getFullName( firstName, lastName ) {
     return '$firstName $lastName';
   }
+  
 }
