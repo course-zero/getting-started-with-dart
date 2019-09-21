e79b37fcc4458e882526b2908700ee8889dd7df7
Uday Hiwarale <thatisuday@gmail.com>
2019-09-21 22:43:19 +0530

    Update 2.break-continue-in-for-loop.dart

--- E.loops/2.break-continue-in-for-loop.dart
+++ E.loops/2.break-continue-in-for-loop.dart
@@ -1,19 +1,24 @@
 import 'dart:io';
 
 /**
- * Keywords `break` and `continue` affects only current loop.
+ * We can skip an interation using `continue` keyword,
+ * or terminate the loop using `break` keyword.
+ 
+ * If we have nested loops,
+ * `break` and `continue` will only affect the current loop.
  */
 void main() {
-
-  // simple `break` and `continue`
+  
+  // [parent]
   for( var i = 0; i < 10; i++ ) {
-    stdout.write( "Parent iteration $i: " ); // always prints
-
+    stdout.write( "Parent iteration $i: " ); // always prints for each iteration of `parent` loop
+    
+    // [child]
     for ( var side in [ 'LEFT', 'RIGHT' ] ) {
       if( side == 'LEFT' ) {
-        continue; // skip iteration of `for-in` for `LEFT`
+        continue; // skip iteration of `child` loop when `side` is `LEFT`
       } else if( i >= 6 ) {
-        break; // break `for-in` loop when `i` is greater than 5
+        break; // break `child` loop when `i` is greater than or equal to 6
       } else {
         stdout.write( "SIDE => $side | " );
       }
