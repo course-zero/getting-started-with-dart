5cb3ca494f85566c347f9f5c0f87e295d10b21d2
Uday Hiwarale <thatisuday@gmail.com>
2019-09-21 22:47:50 +0530

    Update 3.labeled-break-and-continue-in-for-loop.dart

--- E.loops/3.labeled-break-and-continue-in-for-loop.dart
+++ E.loops/3.labeled-break-and-continue-in-for-loop.dart
@@ -1,20 +1,24 @@
 import 'dart:io';
 
 /**
- * We can use `break LABEL` and `continue LABEL` syntax
- * to specify which loop to break or continue.
+ * We can instruct `continue` and `break` keywords to terminat a specific loop,
+ * by providing label of the loop we want continue or break.
+ *
+ * `LABEL: for( ...` syntax assigns the `LABEL` label to the for loop.
+ * To continue, we use `continue LABEL;` and to break, we use `break LABEL;` syntax;
  */
 void main() {
-
-  // simple `break` and `continue`
+  
+  // [PARENT] for loop
   PARENT: for( var i = 0; i < 10; i++ ) {
     stdout.write( "Parent iteration $i: " ); // always prints
-
+    
+    // [CHILD] for-in loop
     CHILD: for ( var side in [ 'LEFT', 'RIGHT' ] ) {
       if( side == 'LEFT' ) {
-        continue CHILD; // skip iteration of CHILD for `LEFT`
+        continue CHILD; // skip iteration of the `CHILD` loop when `side` is `LEFT`
       } else if( i >= 6 ) {
-        break PARENT; // break PARENT loop when `i` is greater than 5
+        break PARENT; // terminate `PARENT` loop when `i` is greater than or equal to `6`
       } else {
         stdout.write( "SIDE => $side | " );
       }
