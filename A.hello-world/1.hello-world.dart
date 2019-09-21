f960f1653d8629a1afd960a24f563c31e15b5193
Uday Hiwarale <thatisuday@gmail.com>
2019-09-21 23:46:02 +0530

    Update 1.hello-world.dart

--- A.hello-world/1.hello-world.dart
+++ A.hello-world/1.hello-world.dart
@@ -1,5 +1,8 @@
-// `main` function is the entry point of execution
-// when a .dart file is executed using `dart` command
+
+/**
+ * `main` function is the entry point of execution
+ * when a .dart file is executed using `dart` command
+ */
 void main() {
   print( "Hello World" ); // print is built-in function to print an object to the STDOUT
 }
