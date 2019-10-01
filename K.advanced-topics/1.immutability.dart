bfcae3db8f159a95f41901d2616cbd25cc5cc4b4
Uday Hiwarale <Uday.Hiwarale>
2019-10-01 23:17:28 +0530

    advanced topics

---
+++ K.advanced-topics/1.immutability.dart
@@ -0,0 +1,27 @@
+import 'package:meta/meta.dart';
+
+/**
+ * @desc `@immutable` annotation from `meta` package forces a class to become immutable.
+ * 
+ * A class is `immutable` if all of the instance fields of the class,
+ * whether defined directly or inherited, are `final`.
+ */
+@immutable
+class User{
+  final String name;
+  
+  User( this.name );
+}
+
+// execute program
+void main() {
+
+  // create a `User` object
+  var u = User( 'John' );
+  
+  // override name property
+  //u.name = 'Mike'; // not allowed because `User` is `immutable`
+  
+  // print name property
+  print( u.name );
+}
